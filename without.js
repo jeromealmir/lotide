9d41cad3ff3b1dd153aa1490d17889d25a06404e
Jerome <jeromelagbas@gmail.com>
2022-07-24 20:01:22 -0700

    fixed code. changed push to splice. created a shallow copy of source array using spread operator

--- without.js
+++ without.js
@@ -26,24 +26,65 @@ const assertArrayEquals =  function(array1, array2) {
   } return result;
 }
 
-//Without function
+//eqArrays function
+
+const eqArrays =  function(array1, array2) {
+  // itemChecker = [];
+  result = '';
+
+  if (array1 === '' || array2 === '') {
+    return checker = false;
+  }
+
+  if (array1.length !== array2.length) {
+    return checker = false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      //checks if individual item matches
+      //itemChecker.push(false);
+      result = false;
+      break //IMPORTANT! breaks the loop!
+    } else {
+      //checks if individual item matches
+      // itemChecker.push(true);
+      result = true;
+    }
+  } return result;
+}
+
+//without function
 
 const without = function (source, itemsToRemove) {
-  let newArray = [];
-  for (let i = 0; i < source.length; i++) {
+  
+  let newArray = [...source];
+
+  for (let i = 0; i < newArray.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
-      if (source[i] !== itemsToRemove[j]) {
-        newArray.push(source[i]);
+      if (source[i] === itemsToRemove[j]) {
+        newArray.splice(i, 1);
+        ;
       }
     }    
   } return newArray;
-}
+};
 
 //Test cases
 
-console.log(without([5, 2, 3], [3]));
+// console.log(without([5, 2, 3], [3]));
+
+// const words = ["hello", "world", "lighthouse"];
+// console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// console.log(assertArrayEquals(words, ["hello", "world", "lighthouse"]));
+
+// console.log(assertArrayEquals(without(["1", "2", "4"], [10, "3", 15, "2"]), ["1", "4"]));
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 console.log(assertArrayEquals(words, ["hello", "world", "lighthouse"]));
