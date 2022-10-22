2ca8b54200c361346ef352333957b86e3e062810
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-10-22 05:10:51 -0700

    create an assert function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// TEST CODE
+assertEqual('Bootcamp', 'Bootcamp');
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(1, 1.1);
