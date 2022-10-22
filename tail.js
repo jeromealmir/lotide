ca470f2bc4b0be7e65c474edfad9167692379a8a
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-10-22 06:01:17 -0700

    create a tail function that returns every element except the first

---
+++ tail.js
@@ -0,0 +1,27 @@
+//ASSERT FUNCTION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// TAIL FUNCTION
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+// TEST CODE
+
+const originalArray = [1, 2, 3];
+const modifiedArray = tail(originalArray);
+
+assertEqual(originalArray, modifiedArray);
+assertEqual(modifiedArray[0], 2);
+assertEqual(modifiedArray[1], 3);
+assertEqual(tail([]), [2, 3]);
+assertEqual(tail([1]), [2, 3]);
+assertEqual(tail([1, 2, 3]), [2, 3]);
