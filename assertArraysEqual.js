f2943446b02b0381fa1ea9c29653dcba4b55221f
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-10-25 06:56:58 -0700

    implement a function that asserts two arrays and log a message to the console

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+//eqArrays Function
+
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//assertArraysEqual Function
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 3, 3]);
