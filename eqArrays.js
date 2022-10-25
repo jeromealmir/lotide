e5ecc1482f4ccc1af49330f6b7fd18201c85acb9
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-10-25 06:39:00 -0700

    implement a function that will assert arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+//ASSERT FUNCTION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+//EQARRAYS FUNCTIOn
+
+const eqArrays = function(array1, array2) {
+  let result = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false
+    }
+  }
+  return true;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(['1', '2', '3'], ['1', '2', '3']); // => true
+eqArrays(['1', '2', '3'], ['1', '2', 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
