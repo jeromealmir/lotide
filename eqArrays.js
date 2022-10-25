f262d645ca7238e86afbe5bb35b6f968337512a2
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-10-25 06:40:01 -0700

    lint code

--- eqArrays.js
+++ eqArrays.js
@@ -11,10 +11,9 @@ const assertEqual = function(actual, expected) {
 //EQARRAYS FUNCTIOn
 
 const eqArrays = function(array1, array2) {
-  let result = true;
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      return false
+      return false;
     }
   }
   return true;
