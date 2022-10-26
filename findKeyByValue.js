c753da6105beef48e28a6387ba5851271add9841
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-10-26 12:44:25 -0700

    implement a function which takes in an object and a value and return the key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+//assertEqual Function
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+//findKeyByValue Function
+
+const findKeyByValue = function(object, value) {
+  let results = undefined;
+  
+  for (const key in object) {
+    if (object[key] === value) {
+      results = key;
+    }
+  }
+  return results;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
