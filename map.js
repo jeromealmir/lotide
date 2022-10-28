e88019a626fd2c875bca1e1fcdd4011af179e247
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-10-28 00:13:46 -0700

    implement own version of map function

---
+++ map.js
@@ -0,0 +1,38 @@
+//eqArrays Function
+
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//assertArraysEqual Function
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+//map Function
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word.toUpperCase())
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ])
+assertArraysEqual(results2, ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"])
