977d2b10c8619459ab26120edc70c3c893b678dc
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-10 07:28:27 -0800

    export eqArrays to module and separate test codes

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,3 @@
-//assertEqual Function
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-//eqArrays Function
-
 const eqArrays = function(actual, expected) {
 
   if ((actual.length !== expected.length)) {
@@ -25,10 +13,4 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(['1', '2', '3'], ['1', '2', '3']); // => true
-eqArrays(['1', '2', '3'], ['1', '2', 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
