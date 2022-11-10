77d3d9fb7bf72b93876baafb38fea43ce004bb9c
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-10 09:04:24 -0800

    convert test codes to mocha test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const originalArray = [1, 2, 3];
-const modifiedArray = tail(originalArray);
-
-assertEqual(originalArray, modifiedArray);
-assertEqual(modifiedArray[0], 2);
-assertEqual(modifiedArray[1], 3);
-assertEqual(tail([]), [2, 3]);
-assertEqual(tail([1]), [2, 3]);
-assertEqual(tail([1, 2, 3]), [2, 3]);
+describe('#tail', () => {
+  it('returns [3] for [2, 3]', () => {
+    assert.deepEqual(tail([2, 3]), [3]);
+  });
+  it('returns [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
