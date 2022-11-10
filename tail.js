eb0c8c4b38a99ec3a96fbd78384286ba9e2318a2
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-10 07:18:20 -0800

    export tail function to module and separate test codes

--- tail.js
+++ tail.js
@@ -1,27 +1,5 @@
-//assertEqual Function
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-//tail Function
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-// TEST CODE
-
-const originalArray = [1, 2, 3];
-const modifiedArray = tail(originalArray);
-
-assertEqual(originalArray, modifiedArray);
-assertEqual(modifiedArray[0], 2);
-assertEqual(modifiedArray[1], 3);
-assertEqual(tail([]), [2, 3]);
-assertEqual(tail([1]), [2, 3]);
-assertEqual(tail([1, 2, 3]), [2, 3]);
+module.exports = tail;
