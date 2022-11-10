494aca2141e18b9f56249f65b5dd223264467899
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-10 13:13:59 -0800

    export function to module and separate test codes

--- countOnly.js
+++ countOnly.js
@@ -1,15 +1,3 @@
-//assertEqual Function
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-//countOnly Function
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
@@ -25,21 +13,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
