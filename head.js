193e2109051a97f0c21584116a9fd852c36978ec
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-10 06:58:17 -0800

    export head function to module and separate test code

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-//assertEqual Function
-
-const assertEqual = require('./assertEqual')
-
-//head Function
-
 const head = function(array) {
   return array.shift();
 }
 
-//TEST CODE
-
-assertEqual(head([5]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+module.exports = head;
