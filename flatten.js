84f0a754d9e501606877c575797b183027cfe763
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-10 13:07:16 -0800

    use recursion to support deep nested arrays

--- flatten.js
+++ flatten.js
@@ -1,39 +1,9 @@
-//eqArrays Function
-
-const eqArrays = function(actual, expected) {
-
-  if ((actual.length !== expected.length)) {
-    return false;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
-
-//assertArraysEqual Function
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-//flatten Function
-
 const flatten = function(array) {
   let flattenedArray = [];
+
   for (const items of array) {
     if (Array.isArray(items)) {
-      for (const item of items) {
-        flattenedArray.push(item);
-      }
+      flattenedArray.push(...flatten(items));
     } else {
       flattenedArray.push(items);
     }
@@ -41,5 +11,4 @@ const flatten = function(array) {
   return flattenedArray;
 };
 
-flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => should PASS
+module.exports = flatten;
