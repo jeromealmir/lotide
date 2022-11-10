ccd3b9b649e2ef94a7f466850eff888f80244dc9
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-10 07:36:26 -0800

    export assertArraysEqual and separate test codes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-//eqArrays Function
-
-const eqArrays = function(actual, expected) {
-
-  if ((actual.length !== expected.length)) {
-    return false;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 //assertArraysEqual Function
 
@@ -25,5 +10,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 3, 3]);
+module.exports = assertArraysEqual;
