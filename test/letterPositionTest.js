27c56c11b22222f07d803198431cdfe2a452f860
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-11 13:50:44 -0800

    convert test codes to mocha test

---
+++ test/letterPositionTest.js
@@ -0,0 +1,14 @@
+const { assert } = require('chai');
+const _ = require('../index');
+
+describe('#letterPositions', () => {
+  it('return {} for " "', () => {
+    assert.deepEqual(_.letterPosition('dog'), { d: [ 0 ], o: [ 1 ], g: [ 2 ] })
+  });
+  it('return { d: [ 0 ], o: [ 1 ], g: [ 2 ] } for "dog"', () => {
+    assert.deepEqual(_.letterPosition('dog'), { d: [ 0 ], o: [ 1 ], g: [ 2 ] })
+  });
+  it('return { h: [ 0 ], e: [ 1 ], l: [ 2, 3 ], o: [ 4 ] } for "hello"', () => {
+    assert.deepEqual(_.letterPosition('hello'), { h: [ 0 ], e: [ 1 ], l: [ 2, 3 ], o: [ 4 ] })
+  });
+});
