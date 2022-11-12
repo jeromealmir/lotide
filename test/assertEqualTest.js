87e37b893b0b3d9c66735e27e69b1c727dacb9fa
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-11 16:24:44 -0800

    print function name and insert new line

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,8 @@
 const _ = require('../index');
 
+console.log('#assertEqual');
 _.assertEqual('Bootcamp', 'Bootcamp');
 _.assertEqual('Lighthouse Labs', 'Bootcamp');
 _.assertEqual(1, 1);
 _.assertEqual(1, 1.1);
+console.log('\n');
