f8595dc9f6f3501562c57785ef164e4077e7d63a
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-11 16:06:58 -0800

    require eqObjects function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,39 +1,4 @@
-//eqArrays Function
-
-const eqArrays = function(actual, expected) {
-
-  if ((actual.length !== expected.length)) {
-    return false;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
-
-//eqObjects Function
-
-const eqObjects = function(object1, object2) {
-
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-
-  for (const key in object1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
-    }
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 //assertObjectsEqual Function
 const assertObjectsEqual = function(actual, expected) {
