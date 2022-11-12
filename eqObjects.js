003f4425da03e07be4ac9644bf4e950394ebc1c9
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-12 14:58:59 -0800

    add support for nested objects using recursion

--- eqObjects.js
+++ eqObjects.js
@@ -10,11 +10,15 @@ const eqObjects = function(object1, object2) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       return eqArrays(object1[key], object2[key]);
     }
+
+    if (typeof object1[key] === 'object') {
+      return eqObjects(object1[key], object2[key]);
+    }
+
     if (object1[key] !== object2[key]) {
       return false;
     }
   }
-  
   return true;
 };
 
