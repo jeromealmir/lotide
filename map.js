5069e6ba1e8cf418d19689254844f5b22f87e7e4
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-11 16:11:13 -0800

    export function to module and separate test codes

--- map.js
+++ map.js
@@ -1,9 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-//map Function
-
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = (array, callback) => {
   const results = [];
   for (let item of array) {
@@ -12,8 +6,4 @@ const map = (array, callback) => {
   return results;
 };
 
-const results1 = map(words, word => word[0]);
-const results2 = map(words, word => word.toUpperCase());
-
-assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
-assertArraysEqual(results2, ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
+module.exports = map;
