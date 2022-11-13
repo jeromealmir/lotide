d2845c685318db763b434221d7a8ce5586767634
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-12 17:04:03 -0800

    add more nested objects test cases

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -21,11 +21,11 @@ describe('#eqObjects', () => {
   it('return true for (objList.cd, objList.dc)', () => {
     assert.strictEqual(_.eqObjects(objList.cd, objList.dc), true);
   });
-  it('return false for (objList.cd, objList.cd2)', () => {
-    assert.strictEqual(_.eqObjects(objList.cd, objList.cd2), false);
+  it('return false for { c: "1", d: ["2", 3], e: [5] }, { c: "1", d: ["2", 3], e: [6, 7] }', () => {
+    assert.strictEqual(_.eqObjects({ c: "1", d: ["2", 3], e: [5] }, { c: "1", d: ["2", 3], e: [6, 7] }), false);
   });
-  it('return false for { a: 2, b: { z: 1 } }, { a: 2, b: { z: 1, ab: 0 } }', () => {
-    assert.strictEqual(_.eqObjects({ a: 2, b: { z: 1 } }, { a: 2, b: { z: 1, ab: 0 } }), false);
+  it('return false for { a: 2, b: { z: 1, ab: 0 }, c: 3 }, { a: 2, b: { z: 1, ab: 0 }, c: {c: 3, e: 0} }', () => {
+    assert.strictEqual(_.eqObjects({ a: 2, b: { z: 1, ab: 0 }, c: 3 }, { a: 2, b: { z: 1, ab: 0 }, c: {c: 3, e: 0} }), false);
   });
   it('return false for { a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }', () => {
     assert.strictEqual(_.eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
