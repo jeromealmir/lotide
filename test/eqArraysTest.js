50c8467272b7eeff153c2b2d4d90ab7dd96f16a7
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-12 16:16:06 -0800

    add test cases including nested arrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -14,4 +14,13 @@ describe('#eqArrays', () => {
   it("return false for ['1', '2', '3'], ['1', '2', 3]", () => {
     assert.deepEqual(_.eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
   });
+  it("return false for [2, [4]], [[2, 3], [4, 5]]", () => {
+    assert.deepEqual(_.eqArrays([2, [4]], [[2, 3], [4, 5]]), false);
+  });
+  it("return true for [[2, 3], [4]], [[2, 3], [4]]", () => {
+    assert.deepEqual(_.eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+  it("return false for [[2, 3], [4]], [[2, 3], 4]", () => {
+    assert.deepEqual(_.eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
 });
