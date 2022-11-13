88e90dd1b1de9d396b0a37a6842fdf5ab92c9d29
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-12 16:15:37 -0800

    add support for nested arrays using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,22 @@
 const eqArrays = function(actual, expected) {
 
+  let results = true;
+  
   if ((actual.length !== expected.length)) {
     return false;
   }
-
+  
   for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
+
+    if (Array.isArray(actual[i])) {
+      if (!eqArrays(actual[i], expected[i])) {
+        return false;
+      }
+    } else if (actual[i] !== expected[i]) {
       return false;
     }
   }
-  
-  return true;
+  return results;
 };
 
 module.exports = eqArrays;
